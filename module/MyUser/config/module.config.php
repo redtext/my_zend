ab1161b6668555e768092b98158144322032210b
root <root@redmine.(none)>
2015-09-04 10:02:01 +0600

    добавил перевод в модуль MyUser

--- module/MyUser/config/module.config.php
+++ module/MyUser/config/module.config.php
@@ -34,4 +34,14 @@ return array(
             ),
         ),
     ),
+    'translator' => array(
+            'locale' => 'ru_RU',
+            'translation_file_patterns' => array(
+                    array(
+                        'type'     => 'gettext',
+                        'base_dir' => __DIR__ . '/../language',
+                        'pattern'  => '%s.mo',
+                        ),
+            ),
+    )
 );
